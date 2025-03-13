6ac12b9a3bdacf7d79cfd1fb914d22c90debb31a
Anuj poudel <anujpoudel54@gmail.com>
2025-03-13 12:15:44 +0545

    refactor vector.h to improve structure and naming conventions

--- vector.h
+++ vector.h
@@ -1,8 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#ifndef VECTOR
-#define VECTOR
+#ifndef VECTOR_n
+#define VECTOR_n
 
 typedef struct
 {
@@ -17,33 +17,45 @@ typedef struct
     int capacity;
 } Vec2Q;
 
-Vec2Q *create_vect2_q(){
-    Vec2Q *q = (Vec2Q*)malloc(sizeof(Vec2Q));
-    q->data = (Vec2*)malloc(sizeof(Vec2) * 3);
+#ifdef USING_VECTOR_IMPL
+#define USING_VECTOR_IMPL
+
+Vec2Q *create_vect2_q()
+{
+    Vec2Q *q = (Vec2Q *)malloc(sizeof(Vec2Q));
+    q->data = (Vec2 *)malloc(sizeof(Vec2) * 3);
     q->f = 0;
     q->b = 0;
     q->capacity = 3;
 }
 
-
-void vec2_enqueue(Vec2Q *q, Vec2 vec){
-    printf("Queueing\n");
-    if (q->f >= q->capacity){
-        q->data = (Vec2*)realloc(q->data, sizeof(Vec2)*q->capacity*2);
+void vec2q_enqueue(Vec2Q *q, Vec2 vec)
+{
+    if (q->f >= q->capacity)
+    {
+        q->data = (Vec2 *)realloc(q->data, sizeof(Vec2) * q->capacity * 2);
         q->capacity *= 2;
     }
     q->data[q->f++] = vec;
 }
 
-Vec2 vec2_dequeue(Vec2Q *q){
-    if (q->b < 0 || q->b >= q->f){
-        return (Vec2){-1,-1};
+int vec2q_is_empty(Vec2Q *q)
+{
+    return q->f <= 0 || q->b == q->f;
+}
+
+Vec2 vec2q_dequeue(Vec2Q *q)
+{
+    if (vec2q_is_empty(q))
+    {
+        return (Vec2){-1, -1};
     }
     return q->data[q->b++];
 }
 
-int vec2_is_empty(Vec2Q *q){
-        return q->f <= 0;
+void vec2q_free(Vec2Q *q)
+{
 }
 
-#endif // VECTOR
+#endif // USING_VECTOR_IMPL
+#endif // VECTOR_n
