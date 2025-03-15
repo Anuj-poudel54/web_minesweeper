afe5c12acf463517f23e09c51cb561e13181d370
Anuj poudel <anujpoudel54@gmail.com>
2025-03-15 18:24:52 +0545

    add new exported functions for bomb and empty cell probability settings

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-emcc -o wasm.js main.c --js-library=lib.js -sEXPORTED_RUNTIME_METHODS="['ccall','cwrap']" -sEXPORTED_FUNCTIONS="['_initialize_game_states','_set_cell_count','_change_cell_values','_is_playing','_reveal_empty_cells','_has_won','_get_cell_at','_get_hint_number_count' ,'_dprint','_malloc','_free']"
+
+emcc -o wasm.js main.c --js-library=lib.js -sEXPORTED_RUNTIME_METHODS="['ccall','cwrap']" -sEXPORTED_FUNCTIONS="['_initialize_game_states','_set_cell_count','_change_cell_values','_is_playing','_has_won','_get_cell_at', '_dprint', '_set_bomb_probability', '_set_empty_cell_probability','_malloc','_free']"
+
